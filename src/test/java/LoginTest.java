05497f97f1714e3286eff55bfc9f8528e52de75d
Ruslan Munasipov <ruslanmunasipov23@gmail.com>
2021-10-15 14:01:21 +0300

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTest {
+    @BeforeAll
+    static void setUp() {
+        Configuration.startMaximized = true;
+    }
+
+    String loginName = "Frankthemurder",
+            password = "4NTGJDTCJD";
+
+    @Test
+    public void main() {
+      open("https://yandex.ru/");
+
+      //xpaths
+        //a[@class = 'home-link desk-notif-card__login-new-item desk-notif-card__login-new-item_enter home-link_black_yes home-link_hover_inherit']//div[@class = 'desk-notif-card__login-new-item-title'] login form
+
+        // заполняем
+
+        $x("//a[@class = 'home-link desk-notif-card__login-new-item desk-notif-card__login-new-item_enter home-link_black_yes home-link_hover_inherit']//div[@class = 'desk-notif-card__login-new-item-title']").click();
+        $x("//span[@class = 'Textinput Textinput_view_big-input Textinput_size_l']//input[@type = 'text']").setValue(loginName);
+        $x("//button[@id = 'passp:sign-in']").click();
+        $x("//input[@id = 'passp-field-passwd']").setValue(password);
+        $x("//button[@id = 'passp:sign-in']").click();
+        System.out.println("123");
+    }
+    }
+
